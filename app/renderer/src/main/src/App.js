0c76527d6aabf40271bf3232e55ed3643c1081c6
Rz <243566431@qq.com>
2021-08-15 14:35:39 +0800

    修改readme文件

--- app/renderer/src/main/src/App.js
+++ app/renderer/src/main/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React ,{ useState, useEffect } from 'react';
 // const { ipcRenderer } = window.require('electron')
-// import { ipcRenderer } from 'electron'
+import { ipcRenderer } from 'electron'
 
 function App() {
+  // 定义属性
+  const [ remoteCode, setRemoteCode ] = useState('')
+  const [ localCode, setLocalCode  ] = useState('')
+  const [ controlText, setControlText ] = useState('')
+  // 登陆函数
+  const login = async () => {
+    /*
+     * 登陆状态在主进程维护 通过ipcReenderer给主进程发起一个消息
+       主进程登陆成功后，会返回一个code
+     */
+
+    let code = ipcRenderer.invoke('login')
+
+    setLocalCode(code)
+  }
+  useEffect(() => {
+    login() // 首先发起登录
+    ipcRenderer.on('control-state-change', handleControlState) // 监听从主进程返回的状态变化
+    return () => {  // 当组件被写在卸载时清理注册的事件 【 相当于 componentWillUnmounted】
+      ipcRenderer.removeListener('control-state-change', handleControlState) 
+    }
+
+  }, [])
+  const handleControlState = () => {}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        controlText === '' ? <>
+          <div> 你的控制码 { localCode }</div>
+          <input type='text' value={ remoteCode } />
+          <button>确认</button>
+        </>: <div> { controlText } </div>
+      }    
     </div>
   );
 }
